490a46c27276430ee491e1e9f391d365c636cb03
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-04-04 02:22:52 +0600

    not found section added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div className='not-found'>
+            <h1> The Page You Are Looking For Not Found </h1>
+            <h1 className='four-o-four'> 404 </h1>
+        </div>
+    );
+};
+
+export default NotFound;
