1e7390948c3bd02c88afe1518caa2a84c87b1084
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-04-04 15:02:52 +0600

    about us included

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>About Us</h2>
+        <div className='container'>
+            <h1 className='about-us'>About Us</h1>
+            <div className='about-information shadow-lg p-3 mb-5 bg-white rounded'>
+                <p>
+                    Name:	Yamaha kabushiki gaisha
+                </p>
+                <p>Formerly:	Nippon Gakki Seizo Company, Limited (1297–1987)</p>
+                <p>Founded:	12 October 1887; 134 years ago</p>
+                <p>Founder:	Torakusu Yamaha</p>
+                <p>Headquarters:	10-1, Nakazawacho, Naka-ku, Hamamatsu, Shizuoka, Japan</p>
+                <p>Area served:	Worldwide</p>
+                <p>Website:	yamaha.com</p>
+
+            </div>
+
         </div>
     );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,13 +3,19 @@ import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div className='ques-ans'>
+        <div className='ques-ans container'>
             <h1 className='ques-ans-heading'>Questions & Answers</h1>
-            <div className='ques-ans-1'>
+            <div className='ques-ans-1 shadow-lg p-3 mb-5 bg-white rounded'>
                 <h4 className='ques-1'>Question-1: What Is Semantic Tag Element?</h4>
                 <h6 className='que-1-paragraph'>
-                    Semantic HTML elements are those that clearly describe their meaning in a human- and machine-readable way.Semantic HTML or semantic markup is HTML that introduces meaning to the web page rather than just presentation. For example, a,p tag indicates that the enclosed text is a paragraph. This is both semantic and presentational because people know what paragraphs are, and browsers know how to display them.The benefit of writing semantic HTML stems from what should be the driving goal of any web page: the desire to communicate. By adding semantic tags to your document, you provide additional information about that document, which aids in communication. Specifically, semantic tags make it clear to the browser what the meaning of a page and its content is.
-                    The semantic elements added in HTML5 are: header, main, article, aside, details, footer.
+                    Ans: Semantic HTML elements are those that clearly define their meaning in a human and machine-readable way. Semantic HTML or semantic marker is HTML that introduces meaning to a web page rather than just a presentation. For example, the tag a, p indicates that a closed text is a paragraph. This is both semantic and presentation because people know what categories, and browsers know how to display them.The benefit of writing semantic HTML comes from what should be the driving goal of any webpage: the desire to communicate. By adding semantic tags to your document, you provide more information about that document, which helps to connect. Specifically, semantic tags make it clear to the browser what the page's meaning and content are.
+                    The semantic elements added in HTML5 are: header, nav, section, main, article, aside, details, footer.
+                </h6>
+            </div>
+            <div className='ques-ans-2 shadow-lg p-3 mb-5 bg-white rounded'>
+                <h4 className='ques-2'>Question-2: What is Context API? What Is the purpose of Context API? </h4>
+                <h6 className='que-2-paragraph'>
+                    Ans: The React Context API is a way for the React application to successfully generate transferable global variables. This is another way of “"prop drilling"” or moving props from grandfather to child to parent, and so on. Content is also regarded as a simple, easy-to-use way for country administrators using Redux. The context provides a way to share similar values ​​between components without explicitly transferring support to all levels of the tree. Context is mainly used when other data needs to be accessed by multiple components at different breeding levels.React.createContext() returns consumer and a provider. The provider is part of the fact that as its names suggest it gives status to its children. It will hold the "store" and be the parent of all the parts that may need that store. The purpose of using Context Api when we need to share state between lot's of components with a lot of nesting. Most of the time, the data we have in one component will only be relevant to its children, so passing it in props is more indicative and nicer. By Using context, we can avoid passing props by through intermediate elements.
                 </h6>
             </div>
 
